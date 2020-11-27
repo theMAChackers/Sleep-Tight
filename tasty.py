a264a06156dab6224d80e473f9c88d3ffd27f2df
geekyvyas <dryashhoney@gmail.com>
2020-11-27 22:23:03 +0530

    Update tasty.py

--- tasty.py
+++ tasty.py
@@ -18,4 +18,8 @@ pickle.dump(res, open("name","wb"))
 id = pickle.load(open("name","rb"))
 time.sleep(8)
 email = firebase.get('/sleep-tight-8a6df/Students/'+ str(id) , 'Name' )
-pyautogui.alert(email)
+confirm = pyautogui.confirm('Is this your Email Id: '+ str(email), buttons=['YES', 'NO'])
+if str(confirm)=='YES':
+    pyautogui.alert('Now Run test.py using the command given in github README.md file.')
+else:
+    pyautogui.alert('Please copy the error code and raise a issue in the repository by pasting the error and the file name.')
