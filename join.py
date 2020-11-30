b6ac3a14ef2d5daf6eebea9dab04119a9ae0ab03
geekyvyas <dryashhoney@gmail.com>
2020-11-30 13:43:43 +0530

    update in main file

---
+++ join.py
@@ -0,0 +1,79 @@
+import pickle
+from firebase import firebase
+import pyautogui
+import time
+
+def space(i):
+    for j in range(0, i):
+         pyautogui.scroll(-100)
+
+def slass(st):
+    id2 = pickle.load(open(st,"rb"))
+    CX = firebase.get('/sleep-tight-8a6df/signin/'+st+'/'+ str(id2) , 'X' )
+    CY = firebase.get('/sleep-tight-8a6df/signin/'+st+'/'+ str(id2) , 'Y' )
+    pyautogui.click(CX, CY)
+
+firebase = firebase.FirebaseApplication('https://sleep-tight-8a6df.firebaseio.com/', None)
+
+
+id2 = pickle.load(open("chrome","rb"))
+X = firebase.get('/sleep-tight-8a6df/Chrome/'+ str(id2) , 'CX' )
+Y = firebase.get('/sleep-tight-8a6df/Chrome/'+ str(id2) , 'CY' )
+pyautogui.click(X, Y)
+
+time.sleep(2)
+pyautogui.write('https://cuchd.blackboard.com/ultra/course')
+pyautogui.keyDown('enter')
+
+time.sleep(10)
+id2 = pickle.load(open("sign","rb"))
+X = firebase.get('/sleep-tight-8a6df/signin/'+ str(id2) , 'SX' )
+Y = firebase.get('/sleep-tight-8a6df/signin/'+ str(id2) , 'SY' )
+pyautogui.click(X, Y)
+
+time.sleep(15)
+
+st = "ELT"
+i = pickle.load(open(st,"rb"))
+space(i)
+slass(st)
+
+pyautogui.alert('After clicking ok move your mouse on join session and wait for another prompt.')
+time.sleep(4)
+currentMouseX, currentMouseY = pyautogui.position()
+
+
+pyautogui.alert('Done!!!')
+time.sleep(2)
+pyautogui.click(currentMouseX, currentMouseY)
+
+data =  { 
+    'X': currentMouseX,
+    'Y': currentMouseY
+    }
+result = firebase.post('/sleep-tight-8a6df/jssion/',data)
+final = ''.join(key + str(val) for key, val in result.items())
+data = str(final)
+proxy = data[4:24]
+pickle.dump(proxy, open("jesi","wb"))
+
+
+
+pyautogui.alert('After clicking ok move your mouse on course room and wait for another prompt.')
+time.sleep(4)
+currentMouseX, currentMouseY = pyautogui.position()
+
+
+pyautogui.alert('Done!!!')
+time.sleep(2)
+
+data =  { 
+    'X': currentMouseX,
+    'Y': currentMouseY
+    }
+result = firebase.post('/sleep-tight-8a6df/jssion1/',data)
+final = ''.join(key + str(val) for key, val in result.items())
+data = str(final)
+proxy = data[4:24]
+pickle.dump(proxy, open("jesin","wb"))
+pyautogui.alert('Now Run inporg.py using the command given in github README.md file.')
--- tropy.py
+++ tropy.py
@@ -10,28 +10,16 @@ def cross():
     buttonx, buttony
     pyautogui.click(buttonx, buttony)
 
-def tab():
-    time.sleep(3)
-    button3333location = pyautogui.locateOnScreen("img/course tab.png")
-    button3333location
-    buttonx, buttony = pyautogui.center(button3333location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
-
 def join():
-    time.sleep(6)
-    button13location = pyautogui.locateOnScreen("img/join session.png")
-    button13location
-    buttonx, buttony = pyautogui.center(button13location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
+    id2 = pickle.load(open("jesi","rb"))
+    X = firebase.get('/sleep-tight-8a6df/jssion/'+ str(id2) , 'X' )
+    Y = firebase.get('/sleep-tight-8a6df/jssion/'+ str(id2) , 'Y' )
+    pyautogui.click(X, Y)
 
-    time.sleep(3)
-    button14location = pyautogui.locateOnScreen("img/course room.png")
-    button14location
-    buttonx, buttony = pyautogui.center(button14location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
+    id2 = pickle.load(open("jesin","rb"))
+    X = firebase.get('/sleep-tight-8a6df/jssion1/'+ str(id2) , 'X' )
+    Y = firebase.get('/sleep-tight-8a6df/jssion1/'+ str(id2) , 'Y' )
+    pyautogui.click(X, Y)
 
 def leave():
     button1234location = pyautogui.locateOnScreen("img/for leave.png")
@@ -54,86 +42,15 @@ def leave():
     buttonx, buttony
     pyautogui.click(buttonx, buttony)
 
-def UCT():
-    time.sleep(2)
-    button411location = pyautogui.locateOnScreen("img/pagepoint.png")
-    button411location
-    buttonx, buttony = pyautogui.center(button411location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
+def space(i):
+    for j in range(0, i):
+         pyautogui.scroll(-100)
 
-    time.sleep(2)
-    pyautogui.scroll(800)
-
-    time.sleep(1)
-    button66location = pyautogui.locateOnScreen("img/communication.png")
-    button66location
-    buttonx, buttony = pyautogui.center(button66location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
-
-def SPT():
-    time.sleep(2)
-    button41location = pyautogui.locateOnScreen("img/pagepoint.png")
-    button41location
-    buttonx, buttony = pyautogui.center(button41location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
-
-    time.sleep(2)
-    pyautogui.scroll(-800)
-
-    time.sleep(1)
-    button42location = pyautogui.locateOnScreen('img\quantum.png')
-    button42location
-    buttonx, buttony = pyautogui.center(button42location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
- 
-
-def ELT():
-    time.sleep(2)
-    button1112location = pyautogui.locateOnScreen("img/pagepoint.png")
-    button1112location
-    buttonx, buttony = pyautogui.center(button1112location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
-
-    time.sleep(2)
-    pyautogui.scroll(250)
-    time.sleep(1)
-    button1222location = pyautogui.locateOnScreen("img/physics electronic.png")
-    button1222location
-    buttonx, buttony = pyautogui.center(button1222location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
-
-def SMT():
-
-    time.sleep(2)
-    button5location = pyautogui.locateOnScreen("img/maths.png")
-    button5location
-    buttonx, buttony = pyautogui.center(button5location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
-
-def MEP():
-    time.sleep(10)
-    button11location = pyautogui.locateOnScreen("img/pagepoint.png")
-    button11location
-    buttonx, buttony = pyautogui.center(button11location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
-
-    time.sleep(2)
-    pyautogui.scroll(-400)
-
-    time.sleep(1)
-    button12location = pyautogui.locateOnScreen("img/autocad.png")
-    button12location
-    buttonx, buttony = pyautogui.center(button12location)
-    buttonx, buttony
-    pyautogui.click(buttonx, buttony)
+def slass(st):
+    id2 = pickle.load(open(st,"rb"))
+    CX = firebase.get('/sleep-tight-8a6df/signin/'+st+'/'+ str(id2) , 'X' )
+    CY = firebase.get('/sleep-tight-8a6df/signin/'+st+'/'+ str(id2) , 'Y' )
+    pyautogui.click(CX, CY)
 
 firebase = firebase.FirebaseApplication('https://sleep-tight-8a6df.firebaseio.com/', None)
 
@@ -155,48 +72,13 @@ pyautogui.click(X, Y)
 
 time.sleep(15)
 
-#friday
-
-#autocad
-MEP()
-join()
-time.sleep(15)
-leave()
-tab()
-cross()
-
-#maths
-SMT()
-join()
-time.sleep(15)
-leave()
-tab()
-cross()
- 
-#quantum and semiconductor
-SPT()
-join()
-time.sleep(15)
-leave()
-tab()
-cross()
-
-
-#communication skills
-UCT()
-join()
-time.sleep(15)
-leave()
-tab()
-cross()
-
-#basic electric and electronic engineering
-ELT()
-join()
-time.sleep(15)
-leave()
-tab()
-cross()
+for j in range(0,6)
+    st = "ELT"
+    i = pickle.load(open(st,"rb"))
+    space(i)
+    slass(st)
+    join()
+    pyautogui.hotkey('alt', 'left')
 
 
 
